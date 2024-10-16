eb0b5e430f6db9e8d3e0bba1066a30a8ec5e7d0c
caioguiibsb <104646702+caioguiibsb@users.noreply.github.com>
2024-10-16 18:49:00 -0300

    correcao desafio 63

--- Aula 14/Desafio 063.py
+++ Aula 14/Desafio 063.py
@@ -1,8 +1,13 @@
 #EScreva um programa que leia um numero n inteiro qualquer e mostre na tela os n primeiros elementos de uma sequencia de fibonacci.
 num = int(input('Digite quantos numeros da sequencia de fibonacci deseja ver: '))
+a=0
+c=0
 i=0
-soma=0
+b=1
 while i <= num:
-    soma = i + (i+1)
-    print(soma)
+    print(f'{c} → ')
+    a=b
+    b=c
+    c=a+b
     i+=1
+print('FIM')
