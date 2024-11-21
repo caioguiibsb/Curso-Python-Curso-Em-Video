83c31db78802a58e75e90c15f6db19a01c242064
caioguiibsb <104646702+caioguiibsb@users.noreply.github.com>
2024-11-21 18:55:50 -0300

    Update Aula 16.py

---
+++ Aula 16/Aula 16.py
@@ -0,0 +1,9 @@
+lanche = ('Hamburguer', 'suco', 'pizza', 'Pudim')
+for cont in range(0, len(lanche)): #aq voce pode pegar a posicao pelo cont
+    print(f"Eu vou comer {lanche[cont]} na posica {cont}")
+
+for comida in lanche: #aq voce apenas pode mostrar o item da posicao
+    print(f"Eu vou comer {comida}")
+
+print(f"Comi pra caramba")
+
