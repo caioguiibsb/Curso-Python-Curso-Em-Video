13b00ae664d35726715eeec7d397d50fa5e22031
caioguiibsb <104646702+caioguiibsb@users.noreply.github.com>
2024-11-25 09:42:03 -0300

    Update Aula 16.py

--- Aula 16/Aula 16.py
+++ Aula 16/Aula 16.py
@@ -16,3 +16,22 @@ for pos, comida in enumerate(lanche):
 
 print(f"Comi pra caramba")
 
+a = (2, 5 ,4)
+b = (5, 8, 1, 2)
+c = b + a
+print(len(c)) #quantidade de elemntos na tupla
+
+a = (2, 5 ,4)
+b = (5, 8, 1, 2)
+c = b + a
+print(len(c.count(5))) #quantas vzs aparece o numero 5 na tupla
+
+a = (2, 5 ,4)
+b = (5, 8, 1, 2)
+c = b + a
+print(len(c.index(8))) #mostra em qual posicao esta o numero 8 (COMEÇANDO DO ZERO)
+print(len(c.index(2, 5))) #mostra em qual posicao esta o numero 2 apatir da 5 posicao
+
+pessoa = ("gustvo", 39, 'M', 99.88)
+del(pessoa) #apaga tupla por completo
+print(pessoa)
