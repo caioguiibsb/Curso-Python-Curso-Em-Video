bb77596e9bad3e96bdb30389500aea00a0b04e27
caioguiibsb <104646702+caioguiibsb@users.noreply.github.com>
2025-01-14 10:50:47 -0300

    Update Desafio 80.py

--- Aula 17/Desafio 80.py
+++ Aula 17/Desafio 80.py
@@ -1,1 +1,16 @@
- 
+lista=[]
+for c in range(0, 5):
+    n = int(input('digite o valor: '))
+    if c == 0 or n > lista[-1]:
+        lista.append(n)
+        print('adicionado ao final da lista...')
+    else:
+        pos = 0
+        while pos < len(lista):
+            if n <= lista[pos]:
+                lista.insert(pos, n)
+                print(f'adicionado a posicao {pos} da lista')
+                break
+            pos+=1
+print('-='*30)
+print(f'os valores digitados em ordem foram {lista}')
